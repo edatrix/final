08260d99879446befd7c95c3c2c58af214bce6c9
edatrix <lespath@gmail.com>
2014-02-23 19:55:54 -0700

    cleans up readme and application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Final
