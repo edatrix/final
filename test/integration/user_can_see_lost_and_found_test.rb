607d9ee66f2d2f4e4f0076bdf5c93df76dea9b81
edatrix <lespath@gmail.com>
2014-02-26 21:01:11 -0700

    normalize image sizes, adds footer, styles form

--- test/integration/user_can_see_lost_and_found_test.rb
+++ test/integration/user_can_see_lost_and_found_test.rb
@@ -4,7 +4,7 @@ class UserTest < Capybara::Rails::TestCase
 
   def test_user_can_see_listings_on_found_page
     visit found_pets_path
-    assert_content page, "Does this belong to you?"
+    assert_content page, "Do I belong to you?"
   end
 
   def test_user_can_see_listings_on_lost_page
