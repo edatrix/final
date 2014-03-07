c3ab8c676ccb52b6d71a09b969f1c4b8d8e280cb
edatrix <lespath@gmail.com>
2014-03-07 10:43:01 -0700

    removes comment

--- app/models/find.rb
+++ app/models/find.rb
@@ -4,7 +4,6 @@ class Find
 
   def initialize
     @petfinder = Petfinder::Client.new('1f7b2c995b345e95a219ee0216c6c2ca', '001140c21ced6653e7812414be872368')
-    # @shelter = Shelter.new(find["shelter"])
   end
 
 
